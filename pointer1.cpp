0cd3049bbd1c1fad527d9a998bb2088a85817a82
Reyanii <indriyaniv9@gmail.com>
2025-05-05 16:23:58 +0800

    menambahkan ref

--- pointer1.cpp
+++ pointer1.cpp
@@ -18,4 +18,22 @@ class Dosen {
 
 
 Dosen &dsref = ds;
-dsref.nama = ""
+dsref.nama = "Joko";
+cout << "alamat memori = " << &dsref << endl;
+dsref.tampilNama();
+
+Dosen *pds = &ds;
+pds->nama = "reza";
+cout << "Alamat memori = " << pds << endl;
+pds->tampilNama;
+
+int a = 5;
+int b 3;
+int *c = &a;
+*c = 9;
+cout << endl;
+cout << a << endl;
+
+cout << "Alamat memori a = " << &a << endl;
+cout << "Alamat memori c = " << c ,, endl;
+cout << "cetak c = " << *c << endl;
