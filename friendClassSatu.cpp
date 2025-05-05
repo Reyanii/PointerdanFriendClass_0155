36a4fb98f305e508ee08e9f40ace4fa28e11947b
Reyanii <indriyaniv9@gmail.com>
2025-05-05 17:08:08 +0800

    menambahkanFileBaru

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -22,3 +22,24 @@ public:
     void setId(int pid);
     void displayAll(orang &a);
 };
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
