52fee3165fd235c0492fafae6b60125060bb9c87
Reyanii <indriyaniv9@gmail.com>
2025-05-05 16:47:34 +0800

    MembuatClass

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class siswa;
+class out_of_range
+{
+private:
+    string nama;
+
+public:
+    void setNama(string nama);
+    friend class siswa;
+};
+
